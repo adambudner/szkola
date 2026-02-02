b6588b76c6cbdf39fae502b91ef23388d9a3e81a
adambudner <adambudner@outlook.com>
2026-02-02 13:44:52 +0100

    .

--- Aplikacje Internetowe/0202/start.js
+++ Aplikacje Internetowe/0202/start.js
@@ -1,2 +1,27 @@
 import {fakerPL} from '@faker-js/faker';
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
+
+const conn = await mysql.createConnection({
+    host    : "localhost",
+    user    : "root",
+    password: "",
+    database: "3tf_100"
+});
+
+let query = `
+    create table if not exists osoby(
+        id int primary key auto_increment,
+        firstName varchar(50),
+        lastName varchar(50),
+        email varchar(70),
+        phone varchar(15)
+    )
+`;
+await conn.query(query)
+
+query = `
+    insert into osoby(firstName, lastName, email, phone) values(?, ?, ?, ?)
+`;
+
+
+await conn.end();
