27061bf3e0bb8bfbdbf899ed48a6c26b90c246a0
adambudner <adambudner@outlook.com>
2026-03-13 19:55:18 +0100

    Create skladnia.cpp

---
+++ powtorki/cpp/Skladnia i zmienne/skladnia.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // biblioteka I/O 
+
+using namespace std; // zaimplementowanie std jako automatycznego dla bazowych funkcji c++
+
+int main()
+{
+	cout << "Hello World" << endl;
+//	cout - wypisanie informacji po << w konsoli;
+//	endl - po wypisaniu tekstu przenosi kursor konsoli do nastêpnej linijki	
+} 
