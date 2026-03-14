a366f1bca17add5b19ae9d0a8868d566fbb3638b
adambudner <adambudner@outlook.com>
2026-03-14 22:57:20 +0100

    Update tablice.cpp

--- powtorki/cpp/Tablice i vectory/tablice.cpp
+++ powtorki/cpp/Tablice i vectory/tablice.cpp
@@ -1,3 +1,51 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
+int main(){
+/*
+	Tablice daj¹ nam mo¿liwoœæ przechowania kilku wartoœci w jednej zmiennej przyk³ad:
+	mamy int a = 1, int b = 10, int c = 23
+	zeby nie zapisywac 3 rozych zmiennych mozemy wytworzyc tablice jednowymiarow¹:
+	i zapisaæ w niej, albo kombinacje wartosci ktore bedziemy u¿ywac albo nasze w³asne wybrane wartoœci
+	
+	sk³adnia tablicy jednowymiarowej:
+	int tablica[iloœæ_pól];
+	w nawiasach kwadratowych dajemy iloœæ wartoœci które bêdziemy chceili przechowaæ
+	czyli je¿eli chcemy przechowaæ 3 wartosci damy [3]
+	
+	ka¿da wartoœæ ma swój index, czyli po³o¿enie w tablicy, indeksowanie w c++ zaczynamy od zera a wiêc
+	je¿eli mamy tablice:
+	int tab[3];
+	tab[0] = 1; // na indeksie 0 wstawiamy wartoœæ 1
+	tab[1] = 2; // na indeksie 1 wstawiamy wartosc 2
+	tab[2] = 3; // na indeksie 2 wstawiamy wartosc 3
+	
+	tablica graficznie wygladla by w taki sposob:
+	1, 2, 3
+	
+	
+	nie mo¿emy daæ indesku [3] poniewa¿ zadeklarowaliœmy i¿ tablica posiada 3 miejsca na wartoœci które ju¿ zape³niliœmy
+	Je¿eli chcemy teraz wyci¹gnaæ liczbe 2 czyli wartoœæ z pola drugiego napiszemy 
+	cout << tabp[1] << endl;
+	
+	Nale¿y pamiêtaæ i¿ wyjœcie indeksem poza miejsce w tablicy wyrzuæi nam bl¹d ju¿ przy kompilacji ;3
+	
+	c++ posaida wymiarów ile chcesz ale skup sie tylko na  tablicach jednowymiarowych i dwuwymiarowych.
+	
+	sk³adnia tablic dwuwymiarowych:
+	int tablica[kolumny][rzedy];
+	w tych nawiasach kwadratowych pierw dajemy ile chcemy kolumn a potem ile rzêdów
+	
+	przyk³adowo
+	int tab[2][2];
+	uznajmy ze wszystkie pola maja wartosc 0
+	
+	graficznie przedstawiono wyglada to tak:
+	0,0
+	0,0
+	
+
+*/
+}
