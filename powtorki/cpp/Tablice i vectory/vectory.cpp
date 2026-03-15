4da8c794ceeb4dee23ad40f80b5ba11caacbbe55
adambudner <adambudner@outlook.com>
2026-03-15 21:58:48 +0100

    Created funckje.cpp, updated vectory.cpp

---
+++ powtorki/cpp/Tablice i vectory/vectory.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector> //to jest biblioteka ktora obslu¿y nam typ vector, czyli taka tablice ale dynamiczn¹(pozniej wytluczone)
+
+using namespace std;
+
+int main(){
+	
+	return 0;
+}
