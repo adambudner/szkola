ac17e69866c63ffd2e8dec1da2673d0063456980
adambudner <adambudner@outlook.com>
2026-09-08 12:03:34 +0200

    odbior

--- Zaawansowane aplikacje webowe/0809/server.js
+++ Zaawansowane aplikacje webowe/0809/server.js
@@ -22,10 +22,14 @@ const server = http.createServer( (req, res)=>{
     console.log("met.:", req.method, "url:", req.url);
     switch (req.method + ";" + req.url) {
         case "GET;/":
-            res.writeHead(200, {"content-type":"text/plain;charset=utf8"});
+            res.writeHead(200, {"content-type":"text/html;charset=utf8"});
             res.end(html);
             break;
-    
+        case "POST;/action1":
+            res.writeHead(200, {"content-type":"text/plain;charset=utf8"});
+            res.end("Serwer odebrał dane od klienta");
+            break;
+
         default:
             res.writeHead(404, {"content-type":"text/plain;charset=utf8"});
             res.end("błędne żądanie");
